071baad586cd10af7eea041b67d335b6db60a099
Ajith <ajithkumar.ns@topgrep.com>
2024-07-02 07:19:54 +0530

    initial commit

---
+++ routes/todo.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const todoRouter = express.Router()
+const todoController = require('../controllers/todo')
+
+todoRouter.get('/', todoController.getAllTodos)
+
+todoRouter.post('/', todoController.createTodo)
+
+todoRouter.patch('/:id', todoController.updateTodo)
+
+todoRouter.delete('/:id', todoController.deleteTodo)
+
+module.exports = todoRouter;
